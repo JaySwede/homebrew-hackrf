00d9014da215f778ed2b39240733c56187742276
Jason Lunt <JaySwede@gmail.com>
2017-04-26 21:13:27 -0600

    remove pyqwt

--- gnuradio.rb
+++ gnuradio.rb
@@ -23,7 +23,7 @@ class Gnuradio < Formula
   depends_on 'libusb'
   depends_on 'orc'
   depends_on 'pyqt' if ARGV.include?('--with-qt')
-  depends_on 'pyqwt' if ARGV.include?('--with-qt')
+  depends_on 'qwt' if ARGV.include?('--with-qt')
   depends_on 'doxygen' if ARGV.include?('--with-docs')
 
   fails_with :clang do
